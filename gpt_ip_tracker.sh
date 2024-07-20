5ee4962f62f389164f87fef32de4140c92bad001
vyacheslav-ku <88930987+vyacheslav-ku@users.noreply.github.com>
2024-07-20 09:09:47 +0400

    Update gpt_ip_tracker.sh

--- gpt_ip_tracker.sh
+++ gpt_ip_tracker.sh
@@ -3,11 +3,14 @@ tunnetwork="10.8.101.0"
 tungw="10.8.101.1"
 istun=$(ip r | grep ${tunnetwork} | wc -l)
 if [ "$istun" -eq "1" ]; then
-  for ipaddr in $(dig +short chatgpt.com)
+  for domain in $(cat dns_store.txt)
   do
-    count_of_rules=$(ip route | grep "${ipaddr}" | wc -l)
-    if [ "$count_of_rules" -eq "0" ]; then
-      ip route add ${ipaddr}/32 via ${tungw}
-    fi
+    for ipaddr in $(dig +short ${domain} | grep '^[.0-9]*$')
+    do
+      count_of_rules=$(ip route | grep "${ipaddr}" | wc -l)
+      if [ "$count_of_rules" -eq "0" ]; then
+        ip route add ${ipaddr}/32 via ${tungw}
+      fi
+    done
   done
 fi
